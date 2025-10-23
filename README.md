e33c7a883cdc418356c1d34aa6fbbdde0d3afd4c
dbc201 <denizbcakiroglu@gmail.com>
2025-10-23 13:17:21 +0300

    add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# password-generator
+
+A GUI password generator written in python.
+
+Exe for windows can be built by installing [pyinstaller](https://pypi.org/project/pyinstaller/) and running ```windows_build.bat```.
--- password_generator.py
+++ password_generator.py
@@ -2,28 +2,43 @@ import tkinter as tk
 from tkinter import messagebox
 import secrets
 import string
+import base64
+import math
 
 
 # Function to generate the password
 def generate_password():
     try:
         length = int(length_entry.get())
-        include_uppercase = uppercase_var.get()
-        include_numbers = numbers_var.get()
-        include_special = special_var.get()
-
-        characters = string.ascii_lowercase
-        if include_uppercase:
-            characters += string.ascii_uppercase
-        if include_numbers:
-            characters += string.digits
-        if include_special:
-            characters += string.punctuation
-
         if length <= 0:
             raise ValueError
+        
+        password = ""
+        
+        if random_bytes_var.get():
 
-        password = ''.join(secrets.choice(characters) for _ in range(length))
+            byte_len = math.ceil(length * 3 / 4)
+            random_bytes = secrets.token_bytes(byte_len)
+
+            password = base64.urlsafe_b64encode(random_bytes).decode('utf-8')
+            password = password[:length]
+
+            # Convert bytes to a base64 encoded string and strip any padding
+            password = base64.urlsafe_b64encode(random_bytes).decode('utf-8')
+        else:
+            include_uppercase = uppercase_var.get()
+            include_numbers = numbers_var.get()
+            include_special = special_var.get()
+
+            characters = string.ascii_lowercase
+            if include_uppercase:
+                characters += string.ascii_uppercase
+            if include_numbers:
+                characters += string.digits
+            if include_special:
+                characters += string.punctuation
+
+            password = ''.join(secrets.choice(characters) for _ in range(length))
         password_entry.delete(0, tk.END)
         password_entry.insert(0, password)
     except ValueError:
@@ -61,6 +76,29 @@ numbers_check.pack(pady=5)
 special_var = tk.BooleanVar(value=True)
 special_check = tk.Checkbutton(window, text="Include Special Characters", variable=special_var)
 special_check.pack(pady=5)
+
+random_bytes_var = tk.BooleanVar(value=False)
+random_bytes_check = tk.Checkbutton(window, text="Use random bytes", variable=random_bytes_var)
+random_bytes_check.pack(pady=5)
+
+def use_random_bytes(*_):
+    use_bytes = random_bytes_var.get()
+
+    # Turn off other options & disable their controls if using bytes
+    if use_bytes:
+        uppercase_var.set(False)
+        numbers_var.set(False)
+        special_var.set(False)
+        uppercase_check.config(state="disabled")
+        numbers_check.config(state="disabled")
+        special_check.config(state="disabled")
+    else:
+        # Re-enable controls; do not force values (user can choose)
+        uppercase_check.config(state="normal")
+        numbers_check.config(state="normal")
+        special_check.config(state="normal")
+
+random_bytes_var.trace_add("write", use_random_bytes)
 
 # Button to generate password
 generate_button = tk.Button(window, text="Generate Password", command=generate_password)
